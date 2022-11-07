45e2804f7682a6981c30b37f4219aa8999306c86
Huynh Bao <ht.bao1112000@gmail.com>
2022-11-07 22:56:11 +0700

    nothing

--- lib/bao.dart
+++ lib/bao.dart
@@ -1,32 +1,9 @@
-import 'dart:convert';
 import 'dart:io';
 
-Future<void> bao(List<String> paths) async {
+Future<void> handleOption(List<String> paths) async {
   if (paths.isEmpty) {
+    stdout.writeln('Option must not empty!');
   } else {
-    for (final path in paths) {
-      var lineNumber = 1;
-      final lines = utf8.decoder
-          .bind(File(path).openRead())
-          .transform(const LineSplitter());
-      try {
-        await for (final line in lines) {
-          if (true) {
-            stdout.write('${lineNumber++} ');
-          }
-          stdout.writeln(line);
-        }
-      } catch (_) {
-        await _handleError(path);
-      }
-    }
-  }
-}
-
-Future<void> _handleError(String path) async {
-  if (await FileSystemEntity.isDirectory(path)) {
-    stderr.writeln('error: $path is a directory');
-  } else {
-    exitCode = 2;
+    if (paths[0] == 'flutter') {}
   }
 }
